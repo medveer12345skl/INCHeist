ec7e3f748051309eef9692efd61370102cac52ee
medveer12345skl <potluri_medhansh_veer@s2025.ssts.edu.sg>
2025-08-25 19:02:44 +0800

    Initial Commit

---
+++ INCHeist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  INCHeist
+//
+//  Created by Potluri Medhansh Veer on 25/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ INCHeist/INCHeistApp.swift
@@ -0,0 +1,17 @@
+//
+//  INCHeistApp.swift
+//  INCHeist
+//
+//  Created by Potluri Medhansh Veer on 25/8/25.
+//
+
+import SwiftUI
+
+@main
+struct INCHeistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
